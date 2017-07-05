81bd0eb619a2909e78db13b76517cab75812d8ad
a-hauger <hauger.anthony@gmail.com>
2017-07-05 10:53:38 -0600

    added textBox.py

---
+++ QT Form/textBox.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class textBoxCreator(QLineEdit):
+	def __init__(self, kindOfWord):
+		QLineEdit.__init__(self)
+
+		self.isModified = 0
+
+		#self.textbox = QLineEdit(self)
+		self.setPlaceholderText(kindOfWord)
+
+	def isTextMod(self):
+		if (self.isModified == 0):
+			return (0)
+		else:
+			return (1)
+
+	def textMod(self):
+		if (self.isModified == 0):
+			self.isModified = 1
+			return
