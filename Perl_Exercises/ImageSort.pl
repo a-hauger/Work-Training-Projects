ac01d315b802d60d40c10fa0b9f29b4cedb91143
a-hauger <hauger.anthony@gmail.com>
2017-07-11 14:18:04 -0600

    Finished ImageSort uploaded

--- Perl_Exercises/ImageSort.pl
+++ Perl_Exercises/ImageSort.pl
@@ -5,34 +5,14 @@ use strict;
 use warnings;
 use Cwd;
 use File::Copy;
-#use Image::Size 'imgsize';
-
-sub sizePNG{
-	return unless $_[0];
-	my ($width, $height);
-	($width, $height) = unpack( "NN", $1) if $_[0] =~ /IHDR(........)/;
-	return ($width, $height);
-}
-
-sub sizeGIF{
-	return unless $_[0];
-	my ($width, $height);
-	($width, $height) = unpack ("SS", $1) if $_[0] =~ /^GIF8..(....)/;
-	return ($width, $height);
-}
-
-sub sizeJPG{
-	return unless $_[0];
-	my ($width, $height);
-	($width, $height) = unpack("nn", $1) if $_[0] =~ /\xFF\xC0...(....)/;
-	return($width, $height);
-}
+use Image::Size 'imgsize';
 
 sub main{
-	#my $img_x = 0;
-	#my $img_y = 0;
+	my $img_x = 0;
+	my $img_y = 0;
 	my @images = ();
 	my $imagePath = "";
+	my $imageDir = "";
 
 	#access directory needing to be sorted
 	my $newDirectory = $ARGV[0];
@@ -41,15 +21,15 @@ sub main{
 	print (getcwd, "\n");
 	
 	#create small/medium/large directories in sorted directory
-	#unless (mkdir "Large_Images"){
-	#	die "Unable to create Large_Images directory.\n";
-	#}
-	#unless (mkdir "Medium_Images"){
-	#	die "Unable to create Medium_Images directory.\n";
-	#}
-	#unless (mkdir "Small_Images"){
-	#	die "Unable to create Small_Images directory.\n";
-	#}
+	unless (mkdir "Large_Images"){
+		die "Unable to create Large_Images directory.\n";
+	}
+	unless (mkdir "Medium_Images"){
+		die "Unable to create Medium_Images directory.\n";
+	}
+	unless (mkdir "Small_Images"){
+		die "Unable to create Small_Images directory.\n";
+	}
 
 	#search for all images in directory
 	opendir(my $currentDirectory, $newDirectory) or die "Can't opendir $newDirectory: $!";
@@ -58,44 +38,32 @@ sub main{
 
 	#access image sizes
 	foreach(@images){
-		
-		my @imageSizes = ();
-		#($img_x, $img_y) = imgsize($_);
+			
+		my @imageSizes = (0, 0);
+		($img_x, $img_y) = imgsize($_);
 
 		$imagePath = join('/', $currentDir, $_);
 		print($imagePath, "\n");
-
-		if ($imagePath =~ /\.png$/){
-			@imageSizes = sizePNG($imagePath);
-			print("@imageSizes\n");
-		}
-		elsif ($imagePath =~ /\.gif$/){
-			@imageSizes = sizeGIF($imagePath);
-			print("@imageSizes\n");
-		}
-		elsif ($imagePath =~ /\.jpg$/){
-			@imageSizes = sizeJPG($imagePath);
-			print("@imageSizes\n");
-		}
+		print ($img_x, " ", $img_y, "\n");
 
 		#determine if image is small/medium/large
 		#place images in correct directories
 
-		#if ($img_x >= 700 or $img_y >= 700){
-		#	#is Large
-		#	my $imageDir = join('/', $currentDir, "Large_Images");
-		#	move($imagePath, $imageDir) or die "Move failed: $!";
-		#}
-		#elsif (($img_x >= 300 && $img_x < 700) or ($img_y >=300 && $img_y < 700){
-		#	#is Medium
-		#	my $imageDir = join('/', $currentDir, "Medium_Images");
-		#	move($imagePath, $imageDir) or die "Move failed: $!";
-		#}
-		#elsif ($img_x < 300 or $img_y < 300){
-		#	#is Small
-		#	my $imageDir = join('/', $currentDir, "Small_Images");
-		#	move($imagePath, $imageDir) or die "Move failed: $!";
-		#}
+		if ($img_x >= 700 or $img_y >= 700){
+			#is Large
+			 $imageDir = join('/', $currentDir, "Large_Images");
+			move($imagePath, $imageDir) or die "Move failed: $!";
+		}
+		elsif (($img_x >= 300 && $img_x < 700) or ($img_y >=300 && $img_y < 700)){
+			#is Medium
+			$imageDir = join('/', $currentDir, "Medium_Images");
+			move($imagePath, $imageDir) or die "Move failed: $!";
+		}
+		elsif ($img_x < 300 or $img_y < 300){
+			#is Small
+			$imageDir = join('/', $currentDir, "Small_Images");
+			move($imagePath, $imageDir) or die "Move failed: $!";
+		}
 	}
 }
 
